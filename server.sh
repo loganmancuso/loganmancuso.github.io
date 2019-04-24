4930d0598c90185c8f719a1a0f6c0717f7710d53
Logan <mickeyacejr@live.com>
2019-04-23 21:17:16 -0400

    upgrade jekyll to 3.6.3

--- server.sh
+++ server.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 
-if [ "$#" -ge 1 ]; then
+if [ "$#" -ne 1 ]; then
 	echo "Illegal Number Of Arguments please use -start/stop"
 	exit
 fi
 
 if [ $1 = "-start" ]; then
-	if [ $2 = "-full" ]; then
-		bundle update
-	fi
+	bundle update
 	bundle exec jekyll serve --incremental --watch
 elif [ $1 = "-stop" ]; then
 	PID=`ps -eaf | grep 'ruby2.5 /usr/local/bin/jekyll serve --incremental' | grep -v grep | awk '{print  $2}'`
