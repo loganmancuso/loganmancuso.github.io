ae448f5330bb9a4c7ae2ab7253fb7a4913f062e9
Logan <mickeyacejr@live.com>
2019-04-25 17:12:34 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch working
     Your branch is up to date with 'origin/working'.
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   _config.yml
    	modified:   _layouts/blog.html
    	modified:   blog.html
    	modified:   blog/main.md
    	modified:   index.html
    	modified:   server.sh

--- server.sh
+++ server.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-
-if [ "$#" -ne 1 ]; then
-	echo "Illegal Number Of Arguments please use -start/stop"
-	exit
-fi
-
-if [ $1 = "-start" ]; then
+if [ "$1" == "-full" ]; then
 	bundle update
-	bundle exec jekyll serve --incremental --watch
-elif [ $1 = "-stop" ]; then
-	PID=`ps -eaf | grep 'ruby2.5 /usr/local/bin/jekyll serve --incremental' | grep -v grep | awk '{print  $2}'`
-	kill -9 $PID
-	echo "killing the server"
-else 
-	echo 'invalid flag, please use -start/stop'
 fi
+
+
+bundle exec jekyll serve --incremental --watch
